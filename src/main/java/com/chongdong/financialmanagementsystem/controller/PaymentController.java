89998265a90490bd0547b502ad8f4a38258c0b4d
黄煜龙 <534411514@qq.com>
2023-09-01 15:59:32 +0800

    fix

--- src/main/java/com/chongdong/financialmanagementsystem/controller/PaymentController.java
+++ src/main/java/com/chongdong/financialmanagementsystem/controller/PaymentController.java
@@ -19,6 +19,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.List;
 
 @RestController
 @RequestMapping("/payment")
@@ -61,13 +63,29 @@ public class PaymentController {
         return paymentService.countPayment();
     }
 
-    @GetMapping("/export")
-    @ResponseBody
-    public void exportExcel(HttpServletResponse response) throws IOException {
-        ExcelUtil.setExcelHeader(response,"支出列表");
-        ServletOutputStream outputStream = response.getOutputStream();
-        ExcelWriter writer = EasyExcel.write(response.getOutputStream()).build();
-        WriteSheet sheet = EasyExcel.writerSheet(0, "sheet").head(Payment.class).build();
-        EasyExcel.write(outputStream).head(Payment.class).excelType(ExcelTypeEnum.XLSX).sheet("用户列表").doWrite(paymentService.exportList(1,10));
+//    @GetMapping("/export")
+//    public void exportExcel(HttpServletResponse response) throws IOException {
+//        ExcelUtil.setExcelHeader(response,"支出列表");
+//        String fileName = URLEncoder.encode("支出列表", "UTF-8").replaceAll("\\+", "%20");
+//        response.setContentType("application/vnd.ms-excel");
+//        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-disposition", "attachment;filename" + fileName + ".xlsx");
+//        ExcelWriter writer = EasyExcel.write(response.getOutputStream()).build();
+//        WriteSheet sheet = EasyExcel.writerSheet(0, "sheet").head(Payment.class).build();
+//        List<Payment> paymentList = paymentService.exportList(1, 10);
+//        writer.write(paymentList,sheet);
+//        writer.finish();
+//        EasyExcel.write(outputStream).head(Payment.class).excelType(ExcelTypeEnum.XLSX).sheet("用户列表").doWrite(paymentService.exportList(1, 10));
+//    }
+
+    @GetMapping("/download")
+    public void download(HttpServletResponse response) throws IOException {
+        // 这里注意 有同学反应使用swagger 会导致各种问题，请直接用浏览器或者用postman
+        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        response.setCharacterEncoding("utf-8");
+        // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+        String fileName=URLEncoder.encode("测试","UTF-8").replaceAll("\\+","%20");
+        response.setHeader("Content-disposition","attachment;filename*=utf-8''"+fileName+".xlsx");
+        EasyExcel.write(response.getOutputStream(),Payment.class).sheet("模板").doWrite(paymentService.exportList(1, 10));
     }
 }
