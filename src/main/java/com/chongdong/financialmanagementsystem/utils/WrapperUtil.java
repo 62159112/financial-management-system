024f21bc90c548a27c03db60737d78f781027362
黄煜龙 <534411514@qq.com>
2023-09-02 10:41:07 +0800

    fix

--- src/main/java/com/chongdong/financialmanagementsystem/utils/WrapperUtil.java
+++ src/main/java/com/chongdong/financialmanagementsystem/utils/WrapperUtil.java
@@ -29,20 +29,23 @@ public class WrapperUtil<T> {
      * 搜索通用
      * */
     public QueryWrapper<T> wrapperNormal(String search, String startTime, String endTime) {
-
+        QueryWrapper<T> wrapper = new QueryWrapper<>();
         String start = null;
         String end = null;
-        if (startTime!=null && endTime!=null){
+        if (startTime!=null  && !startTime.equals("") && endTime!=null && !endTime.equals("")){
             start = startTime.contains("/") ? startTime.replaceAll("/", "-") : startTime;
             end = endTime.contains("/") ? endTime.replaceAll("/", "-") : endTime;
+
         }
-        QueryWrapper<T> wrapper = new QueryWrapper<>();
         wrapper.apply(start!=null,"UNIX_TIMESTAMP(create_time) >= UNIX_TIMESTAMP('" + start + "')");
-        wrapper.apply(end!=null,"UNIX_TIMESTAMP(create_time) <= UNIX_TIMESTAMP('" + end + "')")
-                .and(QueryWrapper->QueryWrapper.like(StringUtils.hasLength(search), "name", search)
-                .or().like(StringUtils.hasLength(search), "type", search)
-                .or().like(StringUtils.hasLength(search), "amount", search)
-                .or().like(StringUtils.hasLength(search), "director", search));
+        wrapper.apply(end!=null,"UNIX_TIMESTAMP(create_time) <= UNIX_TIMESTAMP('" + end + "')");
+        if (search != null && !search.equals("")) {
+            wrapper.and(QueryWrapper->QueryWrapper.like( "name", search)
+                    .or().like("type", search)
+                    .or().like( "amount", search)
+                    .or().like("director", search));
+        }
+
         wrapper.orderByDesc("create_time");
         return wrapper;
     }
