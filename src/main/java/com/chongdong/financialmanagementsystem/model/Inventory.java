46cd90422391ecf24ad12ed0774c9ba363086e10
minkai <951286607@qq.com>
2023-09-04 18:00:27 +0800

    库存物品详情数据下载功能

--- src/main/java/com/chongdong/financialmanagementsystem/model/Inventory.java
+++ src/main/java/com/chongdong/financialmanagementsystem/model/Inventory.java
@@ -1,5 +1,9 @@
 package com.chongdong.financialmanagementsystem.model;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -19,36 +23,50 @@ public class Inventory implements Serializable {
      * 库存条目编号
      */
     @TableId(type = IdType.AUTO)
+    @ExcelIgnore
     private Integer id;
 
     /**
      * 库存条目名称
      */
+    @ExcelProperty("库存条目名称")
+    @ColumnWidth(35)
     private String name;
 
     /**
      * 库存条目类型
      */
+    @ExcelProperty("库存条目类型")
+    @ColumnWidth(35)
     private String type;
 
     /**
      * 库存总量
      */
+    @ExcelProperty("库存总量")
+    @ColumnWidth(35)
     private Integer total;
 
     /**
      * 已使用数量
      */
+    @ExcelProperty("已使用数量")
+    @ColumnWidth(35)
     private Integer usedQuantity;
 
     /**
      * 修改时间
      */
+    @ExcelProperty("时间")
+    @DateTimeFormat("yyyy-MM-dd")
+    @ColumnWidth(35)
     private Date updateTime;
 
     /**
      * 负责人
      */
+    @ExcelProperty("负责人")
+    @ColumnWidth(35)
     private String director;
 
     @TableField(exist = false)
--- src/main/java/com/chongdong/financialmanagementsystem/model/InventoryUsage.java
+++ src/main/java/com/chongdong/financialmanagementsystem/model/InventoryUsage.java
@@ -1,5 +1,9 @@
 package com.chongdong.financialmanagementsystem.model;
 
+import com.alibaba.excel.annotation.ExcelIgnore;
+import com.alibaba.excel.annotation.ExcelProperty;
+import com.alibaba.excel.annotation.format.DateTimeFormat;
+import com.alibaba.excel.annotation.write.style.ColumnWidth;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
@@ -19,31 +23,43 @@ public class InventoryUsage implements Serializable {
      * 物品使用详情编号
      */
     @TableId(type = IdType.AUTO)
+    @ExcelIgnore
     private Integer id;
 
     /**
      * 使用人
      */
+    @ExcelProperty("使用人")
+    @ColumnWidth(35)
     private String user;
 
     /**
      * 使用数量
      */
+    @ExcelProperty("使用数量")
+    @ColumnWidth(35)
     private Integer quantity;
 
     /**
      * 使用时间	
      */
+    @ExcelProperty("使用时间")
+    @DateTimeFormat("yyyy-MM-dd")
+    @ColumnWidth(35)
     private Date createTime;
 
     /**
      * 备注
      */
+    @ExcelProperty("备注")
+    @ColumnWidth(35)
     private String remark;
 
     /**
      * 物品名称
      */
+    @ExcelProperty("物品名称")
+    @ColumnWidth(35)
     private String article;
 
     @TableField(exist = false)
